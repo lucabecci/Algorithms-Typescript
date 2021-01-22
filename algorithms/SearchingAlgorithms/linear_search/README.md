e7f6f67aca88dacd4ee8bee692ccb15d8bcf4a9d
luca <beccibrian@gmail.com>
2021-01-22 10:18:01 -0300

    chore(@all): Linear search and binary search added

---
+++ algorithms/SearchingAlgorithms/linear_search/README.md
@@ -0,0 +1,2 @@
+> Problem: Given an array arr[] of n elements, write a function to search a given element x in arr[].
+
---
+++ algorithms/SearchingAlgorithms/linear_search/ls.ts
@@ -0,0 +1,15 @@
+const arr_linear_search: number[] = [20, 30, 20, 80, 30, 60, 50, 10, 100, 130, 170, 2]
+
+function linearSearch(arr: number[], n: number): number {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element === n){
+            return element
+        }
+    }
+    return -1
+}
+
+const result = linearSearch(arr_linear_search, 2)
+
+console.log(result)
